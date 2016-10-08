8833f8521dddf4748a7651b98b72cdea0163f80b
KKDestiny <1139904786@qq.com>
2016-10-08 15:47:41 +0800

    提交代码

---
+++ routes/oauth.js
@@ -0,0 +1,75 @@
+/**
+ * @Module   : Wechat oauth Module
+ * @Brief    : Process Wechat oauth
+ */
+var express = require('express');
+var router = express.Router();
+var request = require('request');
+
+/* 微信登陆 */
+var AppID = '<你的测试号或正式号appid>';
+var AppSecret = '<你的测试号或正式号appsecret>';
+router.get('/wx_login', function(req,res, next){
+    //console.log("oauth - login")
+
+    // 第一步：用户同意授权，获取code
+    var router = 'get_wx_access_token';
+    // 这是编码后的地址
+    var return_uri = 'http%3A%2F%2Fwww.onelib.biz%2Foauth%2F'+router;  
+    var scope = 'snsapi_userinfo';
+
+    res.redirect('https://open.weixin.qq.com/connect/oauth2/authorize?appid='+AppID+'&redirect_uri='+return_uri+'&response_type=code&scope='+scope+'&state=STATE#wechat_redirect');
+
+});
+
+
+router.get('/get_wx_access_token', function(req,res, next){
+    //console.log("get_wx_access_token")
+    //console.log("code_return: "+req.query.code)
+
+    // 第二步：通过code换取网页授权access_token
+    var code = req.query.code;
+    request.get(
+        {   
+            url:'https://api.weixin.qq.com/sns/oauth2/access_token?appid='+AppID+'&secret='+AppSecret+'&code='+code+'&grant_type=authorization_code',
+        },
+        function(error, response, body){
+            if(response.statusCode == 200){
+
+                // 第三步：拉取用户信息(需scope为 snsapi_userinfo)
+                //console.log(JSON.parse(body));
+                var data = JSON.parse(body);
+                var access_token = data.access_token;
+                var openid = data.openid;
+
+                request.get(
+                    {
+                        url:'https://api.weixin.qq.com/sns/userinfo?access_token='+access_token+'&openid='+openid+'&lang=zh_CN',
+                    },
+                    function(error, response, body){
+                        if(response.statusCode == 200){
+
+                            // 第四步：根据获取的用户信息进行对应操作
+                            var userinfo = JSON.parse(body);
+                            //console.log(JSON.parse(body));
+                            console.log('获取微信信息成功！');
+
+                            // 小测试，实际应用中，可以由此创建一个帐户
+                            res.send("\
+                                <h1>"+userinfo.nickname+" 的个人信息</h1>\
+                                <p><img src='"+userinfo.headimgurl+"' /></p>\
+                                <p>"+userinfo.city+"，"+userinfo.province+"，"+userinfo.country+"</p>\
+                            ");
+
+                        }else{
+                            console.log(response.statusCode);
+                        }
+                    }
+                );
+            }else{
+                console.log(response.statusCode);
+            }
+        }
+    );
+});
+module.exports = router;
